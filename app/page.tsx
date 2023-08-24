778f26d3d63d465330f9e4b41f5707df6da347a1
johnmuthua <caveomuthama@gmail.com>
2023-08-24 19:29:31 +0300

    Add sessions to server

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1>Dashboard</h1>;
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  return (
+    <main>
+      <div className="">Sessions</div>
+      <pre>{JSON.stringify(session)}</pre>
+    </main>
+  );
 }
