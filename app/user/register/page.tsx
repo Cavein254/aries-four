2c3ee419b7aeb405e12631550db93dcab7ef2830
johnmuthua <caveomuthama@gmail.com>
2023-08-24 20:08:33 +0300

    chore handled routing

---
+++ app/user/register/page.tsx
@@ -0,0 +1,11 @@
+import Register from '../../components/register/Register';
+
+const registerpage = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default registerpage;
