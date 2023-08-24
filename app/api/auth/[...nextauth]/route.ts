83e1631ad001be9ef9a8fac20e6ca111bc0a145e
johnmuthua <caveomuthama@gmail.com>
2023-08-24 19:10:09 +0300

    Add basic authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import { type NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign In',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'user@email.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'password',
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: '1', name: 'Ethan', email: 'test@test.com' };
+        return user;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
