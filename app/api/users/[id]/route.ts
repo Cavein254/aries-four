77ad19eb7e3feb33d5e81272ce9ac8e5cabed29d
johnmuthua <caveomuthama@gmail.com>
2023-08-25 10:51:06 +0300

    Add crud otions to user

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,54 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!user) {
+    return new NextResponse('The user does not exist', {
+      status: 404,
+    });
+  }
+  return NextResponse.json(user);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  const data = await request.json();
+  const user = await prisma.user.update({
+    where: { id },
+    data,
+  });
+  if (!user) {
+    return new NextResponse('The user does not exist', {
+      status: 404,
+    });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    const data = await request.json();
+    await prisma.user.delete({
+      where: { id },
+    });
+
+    return new NextResponse('User Deleted successfully', {
+      status: 204,
+    });
+  } catch (err) {}
+}
