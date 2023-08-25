25ff520d2fd9e10aae8a674c02c7187253f7ca38
johnmuthua <caveomuthama@gmail.com>
2023-08-25 10:32:39 +0300

    Add create user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,4 +6,24 @@ export async function GET(request: Request) {
   return NextResponse.json(users);
 }
 
-export async function POST(request: Request) {}
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    const user = await prisma.user.create({
+      data,
+    });
+    return new NextResponse(JSON.stringify(user), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (err) {
+    if (err.code === 'P2002') {
+      return new NextResponse('The email already exists', {
+        status: 409,
+      });
+    }
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+}
